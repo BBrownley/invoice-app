a61edafa4743d025514bd45b599889c0a757430c
BBrownley <brendobrownley@yahoo.com>
2021-04-20 23:21:34 -0400

    Added warning for when there are no invoices to display

--- app/src/components/InvoicesList/InvoicesList.elements.js
+++ app/src/components/InvoicesList/InvoicesList.elements.js
@@ -1,3 +1,28 @@
 import styled from "styled-components";
+import imgEmpty from "../../assets/illustration-empty.svg";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  text-align: center;
+`;
+
+export const EmptyWarning = styled.div`
+  width: 240px;
+  margin: 63px auto 0 auto;
+  color: ${props => props.theme.colors.gray500};
+  p {
+    margin-top: 1.75rem;
+    strong {
+      font-weight: bold;
+    }
+  }
+  h2 {
+    color: ${props => props.theme.colors.black600};
+  }
+`;
+
+export const EmptyImg = styled.div`
+  background-image: url(${imgEmpty});
+  background-size: cover;
+  height: 198px;
+  margin-bottom: 70px;
+`;
--- app/src/components/InvoicesList/InvoicesList.jsx
+++ app/src/components/InvoicesList/InvoicesList.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Invoice from "../Invoice/Invoice";
+import imgEmpty from "../../assets/illustration-empty.svg";
 
-import { Container } from "./InvoicesList.elements";
+import { Container, EmptyWarning, EmptyImg } from "./InvoicesList.elements";
 
 export default function InvoicesList({ invoices }) {
   return (
     <Container>
+      {invoices.length === 0 && (
+        <EmptyWarning>
+          <EmptyImg></EmptyImg>
+          <h2>There is nothing here</h2>
+          <p>
+            Create an invoice by clicking the <strong>New Invoice</strong>{" "}
+            button and get started. Alternatively, toggle the various status
+            filters to view invoices that were filtered out.
+          </p>
+        </EmptyWarning>
+      )}
       {invoices.map((invoice, key) => {
         return <Invoice invoice={invoice} key={key} />;
       })}
