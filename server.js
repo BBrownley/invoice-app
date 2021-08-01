ee7bdf28e2d4a7c31d5f6d26c968de686354a69d
BBrownley <brendobrownley@yahoo.com>
2021-08-01 15:40:05 -0400

    .

---
+++ server.js
@@ -0,0 +1,70 @@
+require("dotenv").config({ path: "./config.env" });
+
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+let PORT;
+
+if (process.env.NODE_ENV === "test") {
+  PORT = process.env.TEST_POST;
+} else {
+  PORT = process.env.PORT || 5000;
+}
+
+const uri =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+
+const connection = require("./server/db/connection");
+const cors = require("cors");
+const tokenExtractor = require("./server/middleware/tokenExtractor");
+
+app.use(cors());
+app.use(express.json());
+app.use(tokenExtractor);
+
+const invoiceRouter = require("./server/controllers/invoices");
+const userRouter = require("./server/controllers/users");
+const errorHandler = require("./server/utils/errorHandler");
+
+app.use("/invoices", invoiceRouter);
+app.use("/users", userRouter);
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(err => {
+    console.log("error connecting to MongoDB: ", err.message);
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static("./app/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, ".", "app", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+  console.log(connection);
+});
+
+app.use(errorHandler);
+
+module.exports = app;
